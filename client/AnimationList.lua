8ba3d8e64dcbb47ba5e50bf0e5df6a6cd1dcd3ae
SKITTLE <92496158+SKITTLE6969@users.noreply.github.com>
2022-07-14 23:44:50 +0530

    Update AnimationList.lua

--- client/AnimationList.lua
+++ client/AnimationList.lua
@@ -2806,6 +2806,41 @@ DP.PropEmotes = {
         EmoteLoop = true,
         EmoteMoving = true,
     } },
+    
+    ["bubbletea"] = {"amb@world_human_drinking@coffee@male@idle_a", "idle_c", "", AnimationOptions =
+   {
+       Prop = 'apa_prop_cs_plastic_cup_01',
+       PropBone = 28422,
+       PropPlacement = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+       EmoteLoop = true,
+       EmoteMoving = true,
+   }},
+   ["misosoup"] = {"amb@world_human_drinking@coffee@male@idle_a", "idle_c", "", AnimationOptions =
+   {
+       Prop = 'v_ret_247_noodle1',
+       PropBone = 28422,
+       PropPlacement = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+       EmoteLoop = true,
+       EmoteMoving = true,
+   }},
+   ["uwusandy"] = {"mp_player_inteat@burger", "mp_player_int_eat_burger", "", AnimationOptions =
+   {
+       Prop = 'ng_proc_food_ornge1a',
+       PropBone = 18905,
+       PropPlacement = {0.13, 0.05, 0.02, -50.0, 16.0, 60.0},
+       EmoteMoving = true,
+   }},
+   ["budhabowl"] = {"anim@scripted@island@special_peds@pavel@hs4_pavel_ig5_caviar_p1", "base_idle", "", AnimationOptions =
+   {
+       Prop = "prop_cs_bowl_01b",
+       PropBone = 60309,
+       PropPlacement = {0.0, 0.0300, 0.0100, 0.0, 0.0, 0.0},
+       SecondProp = 'h4_prop_h4_caviar_spoon_01a',
+       SecondPropBone = 28422,
+       SecondPropPlacement = {0.0,0.0,0.0,0.0,0.0,0.0},
+       EmoteLoop = true,
+       EmoteMoving = true,
+   }},
 
     -----------------------------------------------------------------------------------------------------
     ------ This is an example of an emote with 2 props, pretty simple! ----------------------------------
@@ -2830,6 +2865,12 @@ DP.PropEmotes = {
         EmoteLoop = true,
         EmoteMoving = true,
     } },
+    ["bong"] = {"anim@safehouse@bong", "bong_stage3", "Bong", AnimationOptions =
+   {
+       Prop = 'hei_heist_sh_bong_01',
+       PropBone = 18905,
+       PropPlacement = {0.10,-0.25,0.0,95.0,190.0,180.0},
+   }},
     ["rose"] = { "anim@heists@humane_labs@finale@keycards", "ped_a_enter_loop", "Rose", AnimationOptions = {
         Prop = "prop_single_rose",
         PropBone = 18905,
